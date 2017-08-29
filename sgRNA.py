9e219c5628075a2e91a03362d0064e263b789dff
JA <justin.ashworth@uts.edu.au>
2017-08-29 12:32:00 +1000

    refactoring, fixes (regex now overlapping)

--- sgRNA.py
+++ sgRNA.py
@@ -10,11 +10,13 @@
 # restriction_sites file format is just multiple lines like "EcoRI GAATTC" (additional file provided)
 
 from shutil import copyfile
-import sys,re,os
+import sys,os,re
 import subprocess
 from subprocess import Popen
 from optparse import OptionParser
+# total_ordering extends custom __eq__ and __lt__ class operators to be sufficient for sorting
 from functools import total_ordering
+import pickle
 
 def msg(_msg): sys.stderr.write('%s\n' %_msg)
 
@@ -61,6 +63,9 @@ def mismatch_count(a,b):
 		if a[i] != b[i]: cnt+=1
 	return cnt
 
+# avoid crRNAs with an early RNA polymerase III termination signal (TTTT), as they will not even be transcribed!
+re_pol_iii = re.compile('TTTT')
+
 class Nuclease:
 	def __init__(self,name,pam,tgtlen,cutpos,high_fidelity_positions,us=4,ds=3):
 		self.name = name
@@ -75,7 +80,34 @@ class Nuclease:
 	def make_re(self):
 		pam = ntexpand(self.pam)
 		# certain scoring metrics (Doench et al.) include additional sequence
-		self.re = re.compile('([ACGT]{%i})(([ACGT]{%i})%s)([ACGT]{%i})' %(self.us,self.tgtlen,pam,self.ds))
+		# the lookahead assertion (?=) evidently allows overlapping matches for Python regex (crucial!)
+		self.re = re.compile('(?=(([ACGT]{%i})(([ACGT]{%i})%s)([ACGT]{%i})))' %(self.us,self.tgtlen,pam,self.ds))
+	def findsites_onestrand(self,searchseq,seqn,strandstr):
+		sites = []
+		for s in self.re.finditer(searchseq):
+			(fullseq,us,seq,cr,ds) = s.groups()
+			# paranoid simple look-back to make sure Python regex/lookahead found a real site
+			if not re.search(fullseq,searchseq):
+				msg('ERROR: regex %s for %s constructed/returned a fictitious full site seq %s [fix regex!]' %(self.re.pattern,self.name,fullseq))
+				sys.exit()
+			# skip sites with an early RNA polymerase III termination signal (TTTT)
+			if re_pol_iii.search(seq):
+				msg('matching sequence with early RNA pol III terminator (TTTT) skipped %s %i %i %s' %(seq,s.start(),s.end(),strandstr))
+				continue
+			site = NucSite(seqn,self.name,seq,cr,s.start(),strandstr)
+			# certain scoring metrics (Doench et al.) include additional sequence
+			site.us = us
+			site.ds = ds
+			for r_e in REs:
+				if r_e.cuts(cr): site.REsites.append(str(r_e))
+			sites.append(site)
+		return sites
+	def findsites(self,searchseq,seqn):
+		sites = []
+		# seqn and strand char are passed here merely for internal site naming purposes
+		sites.extend( self.findsites_onestrand(searchseq,seqn,'+') )
+		sites.extend( self.findsites_onestrand(rvs_comp_str(searchseq),seqn,'-') )
+		return sites
 	def tolerates(self,match,tgt):
 		for p in self.high_fidelity_positions:
 			if match[p] != tgt[p]: return False
@@ -90,32 +122,85 @@ nucleases = {
 	# add more here as desired
 }
 
+@total_ordering
+class Site:
+	sep = '\t'
+	def __init__(self,seq,start,strand):
+		self.seq      = seq
+		self.start    = start
+		self.strand   = strand
+	def __len__(self):
+		return len(self.seq)
+	def __str__(self):
+		out = [self.seq,str(self.start),str(self.start+len(self.seq)),self.strand]
+		return self.sep.join(out)
+	# following operators are for storing & sorting members of this class in lists and dicts
+	def __key(self): return (self.seq, self.start, self.strand)
+	def __hash__(self): return hash(self.__key())
+	def __eq__(self,other): return self.__key() == other.__key()
+	def __lt__(self,other):
+		if self.start < other.start: return True
+		if self.start > other.start: return False
+		if self.strand == '-' and other.strand == '+': return True
+		if self.strand == '+' and other.strand == '-': return False
+		return self.seq < other.seq
+
+@total_ordering
+class NucSite(Site):
+	def __init__(self,parent,nuclease,seq,sgRNA,start,strand):
+		Site.__init__(self,seq,start,strand)
+		self.parent   = parent
+		self.nuclease = nuclease
+		self.sgRNA    = sgRNA
+		self.REsites  = []
+		self.matches  = []
+		self.accepted_matches  = []
+	def seqmatch(self,otherseq):
+		if self.seq.upper() == otherseq.upper(): return True
+		return False
+	def name(self):
+		return '%s_%s_%i_%s' %(self.parent,self.nuclease,self.start,self.strand)
+	def __str__(self):
+		out = ['TGT',self.name(),self.parent,self.seq,self.sgRNA,str(self.start),str(self.start+len(self.seq)),self.strand]
+		if hasattr(self,'ontarget'):
+			out.append('%0.2f' %self.ontarget)
+		out.append(','.join(self.REsites))
+		return self.sep.join(out)
+	# following operators are for storing & sorting members of this class in lists and dicts
+	def __key(self): return (self.parent, self.nuclease, self.seq, self.start, self.strand)
+	def __hash__(self): return hash(self.__key())
+	def __eq__(self,other): return self.__key() == other.__key()
+	def __lt__(self,other):
+		if self.parent < other.parent: return True
+		if self.parent > other.parent: return False
+		if self.nuclease < other.nuclease: return True
+		if self.nuclease > other.nuclease: return False
+		if self.start < other.start: return True
+		if self.start > other.start: return False
+		if self.strand == '-' and other.strand == '+': return True
+		if self.strand == '+' and other.strand == '-': return False
+		return self.seq < other.seq
+
 # this line instructs compiler to autoexpand __lt__ and __eq__ into full set of comparison operators
 @total_ordering
-class SiteMatch:
-	def __init__(self,gnm,sq,start,end,strand):
+class SiteMatch(Site):
+	def __init__(self,gnm,seq,start,strand):
+		Site.__init__(self,seq,start,strand)
 		self.gnm       = gnm
-		self.sq        = sq
-		self.start     = start
-		self.end       = end
-		self.strand    = strand
 		self.mismatches = -1
 		self.tolerated = False
 		self.intronic  = False
 	def __str__(self):
-		out = [self.gnm,self.sq,str(self.start),str(self.end),self.strand,str(self.mismatches)]
-
+		out = [self.gnm,self.seq,str(self.start),str(self.start+len(self.seq)),self.strand,str(self.mismatches)]
 		if self.tolerated: out.append('***TOLERATED***')
 		else: out.append('')
-
 		if self.intronic: out.append('***INTRONIC***')
 		else: out.append('')
-
 		return '\t'.join(out)
-
-	# following two operators are for sorting members of this class
-	def __eq__(self,other):
-		return self.gnm == other.gnm and self.sq == other.sq and self.start == other.start and self.strand == other.strand
+	# following operators are for storing & sorting members of this class in lists and dicts
+	def __key(self): return (self.gnm, self.seq, self.start, self.strand, self.mismatches, self.tolerated, self.intronic)
+	def __hash__(self): return hash(self.__key())
+	def __eq__(self,other): return self.__key() == other.__key()
 	def __lt__(self,other):
 		if self.tolerated and not other.tolerated: return True
 		if not self.tolerated and other.tolerated: return False
@@ -144,7 +229,7 @@ class CasOFFinder(SiteFinder):
 		for f in self.fastafilestosearch:
 			copyfile(f,'%s/%s' %(self.fastadir,f))
 
-	def get_matches(self,sites,nucleases,maxmis,flags):
+	def get_matches(self,nucsites,nucleases,maxmis,flags):
 		for nuc in nucleases:
 			inputfilename = 'casOFFinder.input.%s' %nuc
 			inputfile = open(inputfilename,'w')
@@ -152,8 +237,8 @@ class CasOFFinder(SiteFinder):
 			inputfile.write('%s\n' %self.fastadir)
 			tgt = ''.join(['N' for i in range(nucleases[nuc].tgtlen)]) + nucleases[nuc].pam
 			inputfile.write('%s\n' %tgt)
-			for site in sites:
-				inputfile.write('%s %i\n' %(sites[site].seq,maxmis))
+			for site in nucsites:
+				inputfile.write('%s %i\n' %(nucsites[site].seq,maxmis))
 			inputfile.close()
 
 			outfilename = '%s.results' %inputfilename
@@ -168,14 +253,13 @@ class CasOFFinder(SiteFinder):
 				seq = f[0]
 				gnm = f[1]
 				gstart  = int(f[2])
-				gsq = f[3]
+				gseq = f[3]
 				gstrand = f[4]
 
 				# cas-offinder doesn't keep track of site names so have to re-match
-				for sitename in sites:
-					if not sites[sitename].seqmatch(seq): continue
-					gend = gstart + len(seq)
-					sites[sitename].matches.append( SiteMatch(gnm,gsq,gstart,gend,gstrand) )
+				for sitename in nucsites:
+					if not nucsites[sitename].seqmatch(seq): continue
+					nucsites[sitename].matches.append( SiteMatch(gnm,gseq,gstart,gstrand) )
 			msg('done parse')
 
 class BlastFinder(SiteFinder):
@@ -191,10 +275,10 @@ class BlastFinder(SiteFinder):
 		mdb.wait()
 		msg('done makeblastdb')
 
-	def get_matches(self,sites,nuclease,maxmis,flags):
+	def get_matches(self,nucsites,nuclease,maxmis,flags):
 		seqs = []
-		for site in sites:
-			seqs.append( (sites[site].name(), sites[site].seq ))
+		for site in nucsites:
+			seqs.append( (nucsites[site].name(), nucsites[site].seq ))
 		sitesf = 'siteseqs.fa'
 		writefasta(seqs,sitesf)
 
@@ -205,20 +289,17 @@ class BlastFinder(SiteFinder):
 			f = h.strip().split()
 			sitename = f[0]
 			gnm = f[1]
-			gsq = f[2]
-			site = sites[sitename]
+			gseq = f[2]
+			site = nucsites[sitename]
 			gstart  = int(f[3])
 			gend    = int(f[4])
 			gstrand = f[5]
 			if gstrand=='plus': gstrand='+'
 			if gstrand=='minus':
 				gstrand='-'
-#				gsq = rvs_comp_str(gsq)
-				temp = gstart
-				# blast flips start end end for minus matches
+				# blast flips start and end for minus matches: fix so start is always 5' on forward strand genome coordinate
 				gstart = gend
-				gend = temp
-			sites[sitename].matches.append( SiteMatch(gnm,gsq,gstart,gend,gstrand) )
+			nucsites[sitename].matches.append( SiteMatch(gnm,gseq,gstart,gstrand) )
 		msg('done parse')
 
 class Scorer:
@@ -227,9 +308,23 @@ class Scorer:
 		self.flags = flags
 
 class CFDScorer(Scorer):
-	re_score = re.compile('Rule set.*: ([0-9.-]+)')
+	testsite = ''.join(['A' for i in range(0,24)]) + 'GGGAAA'
 	def __init__(self,exe,flags=[]):
 		Scorer.__init__(self,exe,flags)
+	def exists(self):
+		test = '%s --seq %s' %(exe,self.testsite)
+		sys.stderr.write('test %s' %test)
+		p=Popen(test,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+		p.wait()
+		if p.returncode==0:
+			score = float(p.stdout.readline().strip().split()[4])
+			sys.stderr.write(' %f (pass)\n' %score)
+			return True
+		else:
+			msg('cfd scorer script %s not found or failed' %exe)
+			msg(p.stdout.read())
+			msg(p.stderr.read())
+			return False
 	def score(self,site):
 		cmd = '%s --seq %s' %(self.exe,''.join([site.us,site.seq,site.ds]))
 		sys.stderr.write(cmd)
@@ -267,34 +362,6 @@ class REnz:
 	def __str__(self):
 		return '%s(%s)' %(self.name,self.site)
 
-class Site:
-	sep = '\t'
-	def __init__(self,parent,nuclease,seq,sgRNA,start,end,strand):
-		self.parent   = parent
-		self.nuclease = nuclease
-		self.seq      = seq
-		self.sgRNA    = sgRNA
-		self.start    = start
-		self.end      = end
-		self.strand   = strand
-		self.REsites  = []
-		self.matches  = []
-		self.accepted_matches  = []
-	def seqmatch(self,seq):
-		su = seq.upper()
-		if su == self.seq: return True
-#		if rvs_comp_str(su) == self.seq: return True
-		return False
-	def __len__(self):
-		return len(self.seq)
-	def name(self):
-		return '%s_%i_%i_%s' %(self.parent,self.start,self.end,self.strand)
-	def __str__(self):
-		out = ['TGT',self.name(),self.nuclease,self.seq,self.sgRNA,str(self.start),str(self.end),self.strand]
-		if hasattr(self,'ontarget'):
-			out.append('%0.2f' %self.ontarget)
-		out.append(','.join(self.REsites))
-		return self.sep.join(out)
 
 ### util functions ###
 def read_restriction_sites(fname):
@@ -342,7 +409,7 @@ if __name__ == "__main__":
 	op.add_option('-r','--refile',default='restriction_enzymes',help='restriction enzymes file in "Name Site" format')
 	op.add_option('-m','--models',default='',help='gene models file in GFF/GTF format (must define intron elements')
 	op.add_option('-a','--algorithm',default='blast',help='algorithm for finding off-target sites; options: blast, casoff')
-	op.add_option('-o','--ontarget',default='cfd',help='algorithm for on-target score (e.g. CFD if you\'ve downloaded it)')
+	op.add_option('-o','--ontarget',default='',help='algorithm for on-target score (e.g. CFD if you\'ve downloaded it)')
 	op.add_option('-e','--evalue',type=float,default=1000,help='blastn evalue (approx: 1=0-2bp, 100=0-3bp, 1000=0-4bp mismatches...)')
 	op.add_option('--maxmis',type=int,default=4,help='maximum mismatches to include when finding off-target sites')
 	opt,args = op.parse_args()
@@ -366,111 +433,72 @@ if __name__ == "__main__":
 
 	seqs = loadfastas(args)
 
-	# skip sites with an early RNA polymerase III termination signal (TTTT)
-	re_pol_iii = re.compile('[T]{4}')
-
-	sites = {}
-	siteorder = []
+	# fill name-lookup-able dict of nuc sites, as random lookup will be needed sometimes (e.g. parsing site-finder output files)
+	nucsites = {}
 	for seqn in sorted(seqs):
 		for nuc in nucleases.values():
-			for s in nuc.re.finditer(seqs[seqn]):
-				(us,sq,cr,ds) = s.groups()
-				# skip sites with an early RNA polymerase III termination signal (TTTT)
-				if re_pol_iii.search(sq):
-					msg('matching sequence with early RNA pol III terminator (TTTT) skipped %s %i %i %s' %(sq,s.start(),s.end(),'+'))
-					continue
-				site = Site(seqn,nuc.name,sq,cr,s.start(),s.end(),'+')
-				# certain scoring metrics (Doench et al.) include additional sequence
-				site.us = us
-				site.ds = ds
-				for r_e in REs:
-					if r_e.cuts(cr): site.REsites.append(str(r_e))
-				sites[site.name()] = site
-				siteorder.append(site.name())
-			for s in nuc.re.finditer(rvs_comp_str(seqs[seqn])):
-				(us,sq,cr,ds) = s.groups()
-				# skip sites with an early RNA polymerase III termination signal (TTTT)
-				if re_pol_iii.search(sq):
-					msg('matching sequence with early RNA pol III terminator (TTTT) skipped %s %i %i %s' %(sq,s.start(),s.end(),'-'))
-					continue
-				site = Site(seqn,nuc.name,sq,cr,s.start(),s.end(),'-')
-				site.us = us
-				site.ds = ds
-				for r_e in REs:
-					if r_e.cuts(cr): site.REsites.append(str(r_e))
-				sites[site.name()] = site
-				siteorder.append(site.name())
+			for s in nuc.findsites(seqs[seqn],seqn):
+				msg(str(s))
+				nucsites[s.name()] = s
+
+	pickle.dump(nucsites,open('nucsites.p','wb'))
 
 	if opt.ontarget == 'cfd':
 		exe = 'rs2_score_calculator.py'
-		scorer_exists = False
-		testsite = ''.join(['A' for i in range(0,24)]) + 'GGGAAA'
-		test = '%s --seq %s' %(exe,testsite)
-		sys.stderr.write('test %s' %test)
-		p=Popen(test,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
-		p.wait()
-		if p.returncode==0:
-			scorer_exists = True
-			sys.stderr.write(' (pass)\n')
-		else:
-			msg('cfd scorer script %s not found or failed' %exe)
-			msg(p.stdout.read())
-			msg(p.stderr.read())
-
-		if scorer_exists:
-			scorer = CFDScorer(exe)
-			for s in sites:
-				if not len(site.seq)==23 or site.nuclease != 'Cas9':
+		scorer = CFDScorer(exe)
+		if scorer.exists():
+			for s in nucsites:
+				if not len(nucsites[s].seq)==23 or nucsites[s].nuclease != 'Cas9':
 					msg('CFDScorer: skipping site %s because CFD score is trained only for 20-mer crRNA-PAM Cas9 sequences [ACGT]{4}[ACGT]{20}NGG[ACGT]{3}')
 					continue
-				sites[s].ontarget = scorer.score(sites[s])
-		else: msg('CFD script %s not found' %exe)
+				nucsites[s].ontarget = scorer.score(nucsites[s])
 
 	# check for off-target sites using finder of choice
 	finder = finders[opt.algorithm]
 	finder.initialize([opt.genomefile])
 	flags = ['-evalue %f' %opt.evalue]
-	finder.get_matches(sites,nucleases,opt.maxmis,flags)
+	finder.get_matches(nucsites,nucleases,opt.maxmis,flags)
 
 	# check/flag matches for nuclease tolerance
 	# check/flag for cuts in intronic sequence
 	# count mismatches, sort
-	for s in sites:
-		site = sites[s]
+	for s in nucsites:
+		site = nucsites[s]
 		nuclease = nucleases[site.nuclease]
 		ntol = 0
 		nclose = 0
-		sites[s].unique=False
+		nucsites[s].unique=False
 		for i in range(len(site.matches)):
 			m = site.matches[i]
-			m.mismatches = mismatch_count(m.sq,site.seq)
+			m.mismatches = mismatch_count(m.seq,site.seq)
 			if m.mismatches > 0 and m.mismatches < 3: nclose+=1
-			if nuclease.tolerates(m.sq, site.seq):
+			if nuclease.tolerates(m.seq, site.seq):
 				# this has to index fully into original dict due to the way that Python doesn't use pointers/references
-				sites[s].matches[i].tolerated=True
+				nucsites[s].matches[i].tolerated=True
 				ntol+=1
 
 			# flag for intronic cut site
-			cut = m.end + nuclease.cutpos
+			cut = m.start + len(m.seq) + nuclease.cutpos
 			if m.strand == '-': cut = m.start+3
 			if m.gnm in introns:
 				for start,end in introns[m.gnm]:
 					if cut >= start and cut <= end:
-						sites[s].matches[i].intronic=True
-		if ntol==1 and nclose==0: sites[s].unique=True
+						nucsites[s].matches[i].intronic=True
+		if ntol==1 and nclose==0: nucsites[s].unique=True
 
 	outroot = 'sites.%s.%s.%i' %(opt.algorithm,opt.genomefile,opt.maxmis)
 	gtf = open('%s.gtf'%outroot,'w')
 	gtfu = open('%s.unique.gtf'%outroot,'w')
-	for s in siteorder:
-		site = sites[s]
+	for s in sorted(nucsites):
+		site = nucsites[s]
 		print(str(site))
 		for m in sorted(site.matches):
 			print('\t'.join(['GNM',site.name(),str(m)]))
 			# GTF output for genome-verified sites
 			if m.mismatches>0: continue
-			gtfline = '%s' %'\t'.join([m.gnm,'JA',site.nuclease,str(m.start),str(m.end),'.',m.strand,'.','parent %s; id %s; seq %s; sgRNA %s; ontarget %s; REsites %s' %(site.name(),site.name(),site.seq,site.sgRNA,site.ontarget,','.join(site.REsites))])
-			gtf.write('%s\n' %gtfline)
+			gtfline = [m.gnm,'JA',site.nuclease,str(m.start),str(m.start+len(m.seq)),'.',m.strand,'.','parent %s; id %s; seq %s; sgRNA %s; REsites %s' %(site.name(),site.name(),site.seq,site.sgRNA,','.join(site.REsites))]
+			if hasattr(m,'ontarget'): gtfline.append('; ontarget %s' %site.ontarget)
+			gtf.write('%s\n' %'\t'.join(gtfline))
 			if site.unique: gtfu.write('%s\n' %gtfline)
 	gtf.close()
 	gtfu.close()
