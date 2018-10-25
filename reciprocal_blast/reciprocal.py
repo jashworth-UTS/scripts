2fb30d4245c24e19f7afb69980cd0b600fd744e5
129880 <129880@ermdc13.itd.uts.edu.au>
2018-10-25 13:42:11 +1100

    .

--- reciprocal_blast/reciprocal.py
+++ reciprocal_blast/reciprocal.py
@@ -4,16 +4,27 @@ import sys
 fa = sys.argv[1]
 fb = sys.argv[2]
 
+# get top blast hits for A
 bestAs = {}
 for l in open(fa):
 	f = l.strip().split()
-	bestAs[f[0]] = f[1]
+	A = f[0]
+	# easy because output sorted, first occurrence sid for qid (first col) is best
+	if not A in bestAs:
+		bestAs[A] = f[1]
 
-# because reciprocal hits are required, can simply filter through 'b' now for 'a' matches
+# get top blast hits for B
+bestBs = {}
 for l in open(fb):
 	f = l.strip().split()
-	a = f[1]
-	b = f[0]
-	if not a in bestAs: continue
-	if bestAs[a] == b:
-		print('%s\t%s' %(a,b))
+	B = f[0]
+	if not B in bestBs:
+		bestBs[B] = f[1]
+
+# output all reciprocal matches
+recip = {}
+for A,B in bestAs.items():
+	if not B in bestBs: continue
+	BA = bestBs[B]
+	if not BA == A: continue
+	print('%s\t%s' %(A,B))
